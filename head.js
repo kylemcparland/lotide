49ec1094ec5567f4f74433fb83e4805eff40f4c7
Kyle McParland <kylemcparland@gmail.com>
2024-06-12 18:23:30 -0400

    Added two more test assertions

--- head.js
+++ head.js
@@ -14,3 +14,6 @@ const head = function (arr) {
 // TEST CODE
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([3]), 5);
+assertEqual(head([]), 5);
+
