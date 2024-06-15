b8098ec25f86e79136721071fc57b11feb899fb7
Kyle McParland <kylemcparland@gmail.com>
2024-06-14 22:05:18 -0400

    Updated eqArrays.js to include AI Feedback.

--- eqArrays.js
+++ eqArrays.js
@@ -7,6 +7,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -18,6 +21,8 @@ const eqArrays = function(arr1, arr2) {
 
 console.log("Arrays match exactly.", eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log("Arrays do not match.", eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log("Arrays do not match. Different lengths.", eqArrays([1, 2, 3, 4], [1, 2, 3])); // => false
 
 console.log("Arrays match exactly.", eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log("Arrays do not match in type.", eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
