2fc347ecbebaf559918fec8bf672929091752242
Kyle McParland <kylemcparland@gmail.com>
2024-06-15 20:02:21 -0400

    Implemented AI feedback

--- middle.js
+++ middle.js
@@ -36,7 +36,16 @@ const middle = function(array) {
   }
   return middleOfArr;
 };
+
+//Test cases:
 console.log("Even array:");
 console.log(middle([1, 2, 3, "middleEven1/2", "middleEven2/2", 6, 7, 8]));
 console.log("Uneven array:");
 console.log(middle([1, 2, "middleOdd", 4, 5]));
+console.log("Array is less than three:")
+console.log(middle([1, 2]));
+console.log("Array is empty:")
+console.log(middle([]));
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2]), []);
