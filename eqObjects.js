e9f87a659c234298ec20045ec5183a10942f2322
Kyle McParland <kylemcparland@gmail.com>
2024-06-17 17:23:22 -0400

    Implemented more AI feedback

--- eqObjects.js
+++ eqObjects.js
@@ -33,20 +33,19 @@ const assertEqual = function(actual, expected) {
 const eqObjects = function(object1, object2) {
   const obj1Keys = Object.keys(object1);
   const obj2Keys = Object.keys(object2);
-  if (obj1Keys.length === obj2Keys.length) {
-    for (let key of obj1Keys) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (eqArrays(object1[key], object2[key]) === false) {
-          return false;
-        }
-      } else if (object1[key] !== object2[key]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
+  if (obj1Keys.length !== obj2Keys.length) {
     return false;
   }
+  for (let key of obj1Keys) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (eqArrays(object1[key], object2[key]) === false) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 //TEST CASES:
