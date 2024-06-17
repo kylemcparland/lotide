6ebe7107ba89340517d36ce8bb4a52033609a4cc
Kyle McParland <kylemcparland@gmail.com>
2024-06-17 17:12:36 -0400

    Added eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,60 @@
+// INITIALIZE eqArrays
+const eqArrays = function(arr1, arr2) {
+  //check if either array is undefined
+  if (arr1 === undefined || arr2 === undefined) {
+    if (arr1 === arr2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  //check if array lengths do not match
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// INITIALIZE assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// INITIALIZE eqObjects
+const eqObjects = function(object1, object2) {
+  const obj1Keys = Object.keys(object1);
+  const obj2Keys = Object.keys(object2);
+  if (obj1Keys.length === obj2Keys.length) {
+    for (let key of obj1Keys) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (eqArrays(object1[key], object2[key]) === false) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+//TEST CASES:
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
+
+const longSleeveMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long",
+};
+eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); // => fals
