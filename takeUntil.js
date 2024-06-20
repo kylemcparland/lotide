19b2d0490993a518d5036fc8763b728715728293
Kyle McParland <kylemcparland@gmail.com>
2024-06-19 22:21:42 -0400

    added takeUntil.js file

---
+++ takeUntil.js
@@ -0,0 +1,64 @@
+// INITIALIZE assertArraysEqual
+const assertArraysEqual = function(arr1, arr2) {
+  let input = eqArrays(arr1, arr2);
+  if (input === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`⛔⛔⛔ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// INITIALIZE eqArrays
+const eqArrays = function(arr1, arr2) {
+  if (arr1 === undefined || arr2 === undefined) {
+    if (arr1 === arr2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// INITIALIZE takeUntil
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let item of array) {
+    if (callback(item) !== false) {
+      return result;
+    }
+    result.push(item);
+  }
+  if (result.length === array.length) {
+    console.log("Value not found. Array returned unedited.");
+    return result;
+  }
+};
+
+// TEST CASES:
+// #1:
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+// #2:
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+
+console.log('---');
+
+// #3:
+const data3 = ["My", "Name", "Is", "Kyle", 2, "McParland"];
+const results3 = takeUntil(data3, x => typeof x === "number");
+assertArraysEqual(results3, ["My", "Name", "Is", "Kyle"]);
