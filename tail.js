e69bd07e6aefa8c574621a53d6f70d17a9065e07
Kyle McParland <kylemcparland@gmail.com>
2024-07-06 14:15:45 -0400

    refactored tail.js to simply use slice()

--- tail.js
+++ tail.js
@@ -1,9 +1,5 @@
 const tail = function(arr) {
-  let headless = [];
-  for (let i = 1; i < arr.length; i++) {
-    headless.push(arr[i]);
-  }
-  return headless;
+  return arr.slice(1);
 };
 
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -2,11 +2,11 @@ const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
 // TEST CODE
-let original = [1, 2, 3, 4, 5, 6, 7];
+const original = [1, 2, 3, 4, 5, 6, 7];
 const result = tail(original);
 assertEqual(result.length, 6);
 assertEqual(result[5], 7);
-assertEqual(result[0], 3);
+assertEqual(result[0], 1);
 // Test case: Original array is unchanged.
 assertEqual(original.length, 7);
 console.log("Array with one element:", tail([1]));
