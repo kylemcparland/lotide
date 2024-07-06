0f351ef72404032d989cc46ef0f55499a6907fda
Kyle McParland <kylemcparland@gmail.com>
2024-07-06 15:44:35 -0400

    refactored middleTest.js test code

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,14 +2,7 @@ const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
 
 // TEST CODE:
-console.log("Even array:");
-console.log(middle([1, 2, 3, "middleEven1/2", "middleEven2/2", 6, 7, 8]));
-console.log("Uneven array:");
-console.log(middle([1, 2, "middleOdd", 4, 5]));
-console.log("Array is less than three:")
-console.log(middle([1, 2]));
-console.log("Array is empty:")
-console.log(middle([]));
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3, "middleEven1/2", "middleEven2/2", 6, 7, 8]), ["middleEven1/2", "middleEven2/2"]); // => Pass. Even array.
+assertArraysEqual(middle([1, 2, "middleOdd", 4, 5]), ["middleOdd"]); // => Pass. Odd array.
+assertArraysEqual(middle([1, 2]), []); // => Pass. Array is 2 or fewer values. Returns empty array.
+assertArraysEqual(middle([]), []); // => Pass. Array is empty. Returns empty array.
