04a66352c0535ee87940881c59ff965a60d73644
Kyle McParland <kylemcparland@gmail.com>
2024-07-06 17:06:27 -0400

    refactored tailTest.js and middleTest.js to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,27 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-// TEST CODE:
-assertArraysEqual(middle([1, 2, 3, "middleEven1/2", "middleEven2/2", 6, 7, 8]), ["middleEven1/2", "middleEven2/2"]); // => Pass. Even array.
-assertArraysEqual(middle([1, 2, "middleOdd", 4, 5]), ["middleOdd"]); // => Pass. Odd array.
-assertArraysEqual(middle([1, 2]), []); // => Pass. Array is 2 or fewer values. Returns empty array.
-assertArraysEqual(middle([]), []); // => Pass. Array is empty. Returns empty array.
+describe("#middle", () => {
+  it("returns ['even1/2', 'even2/2'] for [1, 2, 3, 'even1/2', 'even2/2', 6, 7, 8]", () => {
+    assert.deepEqual(middle([1, 2, 3, "even1/2", "even2/2", 6, 7, 8]), ["even1/2", "even2/2"]);
+  });
+  it("returns ['odd'] for [1, 2, 'odd', 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, "odd", 4, 5]), ["odd"]);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+});
+
+
+// const assertArraysEqual = require("../assertArraysEqual");
+// const middle = require("../middle");
+
+// // TEST CODE:
+// assertArraysEqual(middle([1, 2, 3, "even1/2", "even2/2", 6, 7, 8]), ["even1/2", "even2/2"]); // => Pass. Even array.
+// assertArraysEqual(middle([1, 2, "middleOdd", 4, 5]), ["middleOdd"]); // => Pass. Odd array.
+// assertArraysEqual(middle([1, 2]), []); // => Pass. Array is 2 or fewer values. Returns empty array.
+// assertArraysEqual(middle([]), []); // => Pass. Array is empty. Returns empty array.
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,30 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-// TEST CODE:
-const original = [1, 2, 3, 4, 5, 6, 7];
-const result = tail(original);
+describe("#tail", () => {
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+  it("returns [2] for [1, 2]", () => {
+    assert.deepEqual(tail([1, 2]), [2]);
+  });
+  it("returns ['2', 3, '4'] for [1, '2', 3, '4']", () => {
+    assert.deepEqual(tail([1, '2', 3, '4']), ['2', 3, '4']);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
 
-assertEqual(result.length, 6); // => Pass. Length is 6.
-assertEqual(result[5], 7); // => Pass. 7 is in index 5.
-assertEqual(result[0], 1); // => Fail. 1 is not in index 0.
-assertEqual(original.length, 7); // => Pass. Length is 7.
-assertEqual(tail([1]), [1]); // => Fail. Array with one element returns empty array.
+// const assertEqual = require("../assertEqual");
+// const tail = require("../tail");
+
+// // TEST CODE:
+// const original = [1, 2, 3, 4, 5, 6, 7];
+// const result = tail(original);
+
+// assertEqual(result.length, 6); // => Pass. Length is 6.
+// assertEqual(result[5], 7); // => Pass. 7 is in index 5.
+// assertEqual(result[0], 1); // => Fail. 1 is not in index 0.
+// assertEqual(original.length, 7); // => Pass. Length is 7.
+// assertEqual(tail([1]), [1]); // => Fail. Array with one element returns empty array.
