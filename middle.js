1f10da2c1c9a7963bafefa5f0dc4be1d5a75f60f
Kyle McParland <kylemcparland@gmail.com>
2024-07-06 15:14:43 -0400

    refactored middle.js

--- middle.js
+++ middle.js
@@ -1,10 +1,13 @@
 const middle = function(array) {
   let middleOfArr = [];
-  if (array.length >= 3 && array.length % 2 === 0) {
+  if (array.length <= 2) {
+    return middleOfArr;
+  }
+  if (array.length % 2 === 0) {
     let middleVar = array.length / 2 - 1;
     middleOfArr.push(array[middleVar]);
     middleOfArr.push(array[middleVar + 1]);
-  } else if (array.length >= 3) {
+  } else {
     let middleVar = Math.floor(array.length / 2);
     middleOfArr.push(array[middleVar]);
   }
